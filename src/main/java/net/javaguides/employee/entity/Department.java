9d9267bf59be643dfcc56475124c2b44035bbe95
Raúl Pérez Vicente <rperezv@ayesa.com>
2026-08-14 14:18:50 +0200

    123. Create Department JPA Entity

---
+++ src/main/java/net/javaguides/employee/entity/Department.java
@@ -0,0 +1,41 @@
+package net.javaguides.employee.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * Department
+ * <p>
+ * Created by IntelliJ, Spring Framework Guru.
+ *
+ * @author architecture - pvraul
+ * @version 14/08/2026 - 14:12
+ * @since 1.17
+ */
+@Table(name = "departments")
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Department {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "department_name", nullable = false)
+    private String departmentName;
+
+    @Column(name = "department_description", nullable = false)
+    private String departmentDescription;
+
+}
